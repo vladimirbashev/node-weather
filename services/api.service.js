bfac22b4ac3aa3ed56a8b351c9cdf2f4e1efa387
Volodymyr Bashev <vladimirbashev@gmail.com>
2025-04-08 13:01:43 +0200

    fixes

--- services/api.service.js
+++ services/api.service.js
@@ -27,7 +27,7 @@ const getIcon = (icon) => {
 const getWeather = async (city) => {
 	const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
 	if (!token) {
-		throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]');
+		throw new Error('API key is not set, use -t [API_KEY]');
 	}
 	const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
 		params: {
